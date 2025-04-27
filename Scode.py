c50c4d8aac1cd91edb71c07bb5d57e06ad5af2b9
reyhanehdavoudi <haddadihanie.edu@gmail.com>
2025-04-27 00:33:35 -0700

    select_parents(population)

--- Scode.py
+++ Scode.py
@@ -20,3 +20,8 @@ def fitness(individual):
             if individual[i] == individual[j] or abs(individual[i] - individual[j]) == abs(i - j):
                 clashes += 1
     return clashes
+
+# تابع برای انتخاب والدین
+def select_parents(population):
+    weights = [1 / (1 + fitness(ind)) for ind in population]
+    return random.choices(population, weights=weights, k=2)
